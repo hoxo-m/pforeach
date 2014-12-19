29911b038e2e1b5f1ca1bb961012f5de93d3b777
hoxo-m <hoxo.smile@gmail.com>
2014-12-19 19:31:42 +0900

    [fix] bug on the parameters for npforeach()

--- R/npforeach.R
+++ R/npforeach.R
@@ -3,6 +3,21 @@
 #'@param ... input for pforeach.
 #'
 #'@export
-npforeach <- function(...) {
-  pforeach(..., .parallel=FALSE)
+npforeach <- function(..., .c, .combine=c, 
+                      .cores, .seed=NULL,
+                      .init, .final=NULL, .inorder=TRUE,
+                      .multicombine=FALSE,
+                      .maxcombine=if (.multicombine) 100 else 2,
+                      .errorhandling=c('pass', 'stop', 'remove'),
+                      .packages=NULL, .export=NULL, .noexport=NULL,
+                      .verbose=FALSE) {
+  pforeach(..., .c=.c, .combine=.combine,
+           .parallel=FALSE,
+           .cores=.cores, .seed=.seed,
+           .init=.init, .final=.final, .inorder=.inorder,
+           .multicombine=.multicombine,
+           .maxcombine=.maxcombine,
+           .errorhandling=.errorhandling,
+           .packages=.packages, .export=.export, .noexport=.noexport,
+           .verbose=.verbose)
 }
